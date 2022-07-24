9eac73c44ee8008280a97b2a4555fd0bebcd374d
Humberto <humberto@Humbertos-Air.fritz.box>
2022-07-24 10:19:28 +0200

    create place logic

--- src/main/GameHelper/GameHelper.java
+++ src/main/GameHelper/GameHelper.java
@@ -3,18 +3,53 @@ package src.main.GameHelper;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 //import src.main.Ship.Ship;
 
 public class GameHelper {
 
 
-    public ArrayList<String> placeShip(ArrayList<String> ship){
+    public ArrayList<String> PlaceShip(ArrayList<String> ship){
 
-      ship.add("A2");
-      ship.add("B2");
-      ship.add("C2");
+      //VARs
+      int placeShipUsed = 0;
+      ArrayList<String> charsArray = new ArrayList<String>();
+      charsArray.add("A");
+      charsArray.add("B");
+      charsArray.add("C");
+      charsArray.add("D");
+      charsArray.add("E");
+      charsArray.add("F");
+      charsArray.add("G");
+
+      if(placeShipUsed == 0){
+
+        Random rand = new Random();
+        int intRandom1 = rand.nextInt(5);
+        int intRandom2 = intRandom1 + 1;
+        int intRandom3 = intRandom1 + 2;
+
+        //rand for char
+        int randIntChar = rand.nextInt(7);
+
+        for(String r: charsArray){
+          int indexRe = charsArray.indexOf(r);
+          if(indexRe == randIntChar){
+            String place1 = intRandom1 + r;
+            String place2 = intRandom2 + r;
+            String place3 = intRandom3 + r;
+
+            ship.add(place1);
+            ship.add(place2);
+            ship.add(place3);
+
+          }
+        }
+
+      }
+ 
 
       return ship;
 
